47dca382af770f0d132e42a337347ec784b9bf6d
Yaroslav Zaychenko <delflumen@ukr.net>
2024-01-25 17:49:42 +0200

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import * as fs from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const doesFileExist = async (path) => {
+    try {
+      await fs.access(path);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  if (await doesFileExist("./src/fs/files/fresh.txt")) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.writeFile(
+      "./src/fs/files/fresh.txt",
+      "I am fresh and young",
+      function (err) {
+        if (err) throw err;
+      }
+    );
+  }
 };
 
 await create();
