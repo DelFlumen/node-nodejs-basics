fb0c1ad78defdd3832ee12822cf05e2f9ac2d70d
Yaroslav Zaychenko <delflumen@ukr.net>
2024-01-26 17:44:01 +0200

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
-const calculateHash = async () => {
-    // Write your code here 
+import * as crypto from "crypto";
+import * as fs from "fs";
+
+const calculateHash = () => {
+  const readerStream = fs.createReadStream(
+    "./src/hash/files/fileToCalculateHashFor.txt"
+  );
+  const hash = crypto.createHash("sha256");
+  hash.setEncoding("hex");
+  readerStream.pipe(hash);
+
+  readerStream.on("end", function () {
+    hash.end();
+    console.log(hash.read());
+  });
 };
 
-await calculateHash();
+calculateHash();
