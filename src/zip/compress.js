4093f4a37dae5d63f1a30dd1d6a7d9bd7128eda0
Yaroslav Zaychenko <delflumen@ukr.net>
2024-01-26 19:38:41 +0200

    feat: add zip:decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,11 +2,17 @@ import * as fs from "fs";
 import * as zlib from "zlib";
 
 const compress = () => {
-  var gzip = zlib.createGzip();
-  var rStream = fs.createReadStream("./src/zip/files/fileToCompress.txt");
-  var wStream = fs.createWriteStream("./src/zip/files/archive.gz");
+  const gzip = zlib.createGzip();
+  const rStream = fs.createReadStream("./src/zip/files/fileToCompress.txt");
+  const wStream = fs.createWriteStream("./src/zip/files/archive.gz");
 
   rStream.pipe(gzip).pipe(wStream);
+
+  rStream.on("end", () =>
+    fs.unlink("./src/zip/files/fileToCompress.txt", function (err) {
+      if (err) throw err;
+    })
+  );
 };
 
 compress();
