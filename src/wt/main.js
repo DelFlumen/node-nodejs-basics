a7c2e039e75531ef9eec62ee549a6a9436f33552
Yaroslav Zaychenko <delflumen@ukr.net>
2024-01-27 19:55:23 +0200

    feat: add workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import { Worker, isMainThread, workerData } from "worker_threads";
+import * as os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuNumber = os.cpus().length;
+  const workerPromises = [];
+
+  if (isMainThread) {
+    for (let i = 0; i < cpuNumber; i++) {
+      const worker = new Worker("./src/wt/worker.js", { workerData: 10 + i });
+
+      const workerPromise = new Promise((resolve, reject) => {
+        worker.on("message", resolve);
+        worker.on("error", reject);
+        worker.on("exit", (code) => {
+          if (code !== 0)
+            reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+      });
+
+      // Push the promise to the array
+      workerPromises.push(workerPromise);
+    }
+    // Wait for all worker promises to resolve
+    Promise.all(workerPromises)
+      .then((results) => {
+        const result = results.reduce((acc, curr) => {
+          return [...acc, { status: curr.status, data: curr.data }];
+        }, []);
+
+        console.log(result);
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  } else {
+    console.log(workerData);
+    console.log("Inside Worker!");
+    console.log(isMainThread); // Prints 'false'.
+  }
 };
 
 await performCalculations();
